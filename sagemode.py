db23a7d4ac51f7fbe31829e1f39c563b991465c7
SenRan <senran101604@gmail.com>
2023-11-12 22:56:12 +0800

    added update feature and use Notify as an attribute

--- sagemode.py
+++ sagemode.py
@@ -1,21 +1,27 @@
-# TODO add a function to check for updates from my github repository
+#! /usr/bin/env python3
+"""
+Sagemode: Track and Unveil Online identities across social media platforms.
+"""
 import os
 import re
 import datetime
+import subprocess
 import threading
 import requests
 from argparse import ArgumentParser
+from rich.console import Console
 
 from accessories import Notify
 from sites import sites
 
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
-class Sagemode(Notify):
+class Sagemode:
     def __init__(self, username, found_only=False):
-        super().__init__()
+        self.console = Console()
+        self.notify = Notify
         self.positive_count = 0
         self.username = username
         self.result_file = os.path.join("data", f"{self.username}.txt")
@@ -29,17 +35,17 @@ class Sagemode(Notify):
             # counts simultaneously and prevent race conditions.
             with threading.Lock():
                 self.positive_count += 1
-            self.notify_found(site, url)
+            self.console.print(self.notify.found(site, url))
             with open(self.result_file, "a") as f:
                 f.write(f"{url}\n")
         if not self.found_only:
-            self.notify_not_found(site)
+            self.console.print(self.notify.not_found(site))
 
     def start(self):
         """
         Start the search.
         """
-        self.notify_start(self.username, sites)
+        self.console.print(self.notify.start(self.username, sites))
 
         current_datetime = datetime.datetime.now()
         date = current_datetime.strftime("%m/%d/%Y")
@@ -48,22 +54,32 @@ class Sagemode(Notify):
         with open(self.result_file, "a") as file:
             file.write(f"\n\n{29*'#'} {date}, {time} {29*'#'}\n\n")
 
+        # storage of thread objects
+        # will create 76 threads
         threads = []
 
         try:
-            with self.console.status(f"[*] Searching for target: {self.username}"):
+            with self.console.status(
+                f"[*] Searching for target: {self.username}", spinner="bouncingBall"
+            ):
                 for site, url in sites.items():
+                    # creates a new thread object
                     thread = threading.Thread(target=self.check_site, args=(site, url))
+                    # track the thread objects by storing it in the assigned threads list.
                     threads.append(thread)
+                    # initiate the execution of the thread
                     thread.start()
-
                 for thread in threads:
+                    # waits for each thread to finish before proceeding.
+                    # to avoid output problems and maintain desired order of executions
                     thread.join()
 
             # notify how many sites the username has been found
-            self.notify_positive_res(self.username, self.positive_count)
+            self.console.print(
+                self.notify.positive_res(self.username, self.positive_count)
+            )
             # notify where the result is stored
-            self.notify_stored_result(self.result_file)
+            self.console.print(self.notify.stored_result(self.result_file))
 
         except Exception:
             self.console.print_exception()
@@ -78,15 +94,20 @@ class Sagemode(Notify):
             local_version = __version__
 
             if remote_version != local_version:
-                self.notify_update(local_version, remote_version)
+                self.console.print(self.notify.update(local_version, remote_version))
 
         except Exception as error:
-            self.notify_update_error(error)
+            self.console.print(self.notify.update_error(error))
+
+    def do_update(self):
+        repo_dir = os.path.dirname(os.path.realpath(__file__))
+        # ensure we're performing git command in the local git repo directory
+        os.chdir(repo_dir)
+        subprocess.run(["git", "pull"])
 
 
 def main():
-    # TODO: add a argument to show version info without giving the required
-    # username argument.
+    # create data folder if it doesn't exist
     if not os.path.exists("data"):
         os.mkdir("data")
 
@@ -108,17 +129,22 @@ def main():
         help="Print Sagemode Version",
         action="store_true",
     )
+    parser.add_argument(
+        "-U", "--update", dest="do_update", help="update Sagemode", action="store_true"
+    )
     args = parser.parse_args()
 
     sagemode = Sagemode(args.username, found_only=args.found)
 
-    if args.username != None:
+    # avoid empty username when sagemode run
+    if args.username is not None:
         sagemode.start()
-
     if args.show_version:
-        sagemode.notify_version(__version__)
+        sagemode.console.rule(sagemode.notify.version(__version__))
+    if args.do_update:
+        sagemode.do_update()
 
-
+    # always check for update in every run
     sagemode.check_for_update()
 
 
