5f2e17f6e60f92583be5548b09601f76d94edc56
SenRan <senran101604@gmail.com>
2023-11-17 19:52:05 +0800

    Added Annotations

--- accessories.py
+++ accessories.py
@@ -27,7 +27,7 @@ ascii_art = """
 """
 
 
-def start(banner, delay=0.001):
+def start(banner: str, delay=0.001):
     """
     Parameters:
         banner
@@ -50,31 +50,31 @@ class Notify:
     "A helper class for notifications of Sagemode process"
 
     @staticmethod
-    def start(username, sites) -> str:
+    def start(username: str, number_of_sites) -> str:
         start(ascii_art, delay=0.1)
         if username or sites is not None:
-            return f"[yellow][[bright_red]*[yellow][yellow]] [bright_blue]Searching {len(sites)} sites for target: [bright_yellow]{username}"
+            return f"[yellow][[bright_red]*[yellow][yellow]] [bright_blue]Searching {number_of_sites} sites for target: [bright_yellow]{username}"
 
     # notify the user how many sites the username has been found
     @staticmethod
-    def positive_res(username, count) -> str:
+    def positive_res(username: str, count) -> str:
         return f"\n[yellow][[bright_red]+[yellow]][bright_green] Found [bright_red]{username} [bright_green]in [bright_magenta]{count}[bright_green] sites"
 
     # notify the user where the result is stored
     @staticmethod
-    def stored_result(result_file):
+    def stored_result(result_file: str) -> str:
         return f"[bright_green][[yellow]@[bright_green]] [orange3]Results stored in: [bright_green]{result_file}\n"
 
     @staticmethod
-    def not_found(site):
+    def not_found(site: str) -> str:
         return f"[black][[red]-[black]] [blue]{site}: [yellow]Not Found!"
 
     @staticmethod
-    def found(site, url):
+    def found(site: str, url: str) -> str:
         return f"[red][[green]+[red]] [green]{site}: [blue]{url}"
 
     @staticmethod
-    def update(local_version, remote_version):
+    def update(local_version: str, remote_version: str) -> str:
         return (
             "[red][[bright_red]![red]] [yellow]Update Available!\n[/yellow]"
             + f"[red][[yellow]![red]] [bright_yellow]You are running Version: [bright_green]{local_version}\n"
@@ -82,11 +82,11 @@ class Notify:
         )
 
     @staticmethod
-    def update_error(error):
+    def update_error(error: str) -> str:
         return f"[bright_red][[bright_red]![bright_red]] [bright_yellow]A problem occured while checking for an update: [bright_red]{error}"
 
     @staticmethod
-    def version(version):
+    def version(version: str) -> str:
         return f"[bright_yellow]Sagemode [bright_red]{version}"
 
 
