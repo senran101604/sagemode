7c87980fde6fcf7d15a0599e6a5a11942ce329e6
SenRan <senran101604@gmail.com>
2023-11-18 20:18:32 +0800

    Added a list of User Agents

--- sites.py
+++ sites.py
@@ -3,26 +3,22 @@ sites = {
     "Facebook": "https://www.facebook.com/{}",
     "Twitter": "https://twitter.com/{}",
     "Instagram": "https://www.instagram.com/{}",
-    "LinkedIn": "https://www.linkedin.com/in/{}",
     "YouTube": "https://www.youtube.com/{}",
     "Snapchat": "https://www.snapchat.com/add/{}",
     "TikTok": "https://www.tiktok.com/@{}",
-    "Discord": "https://discord.com/users/{}",
     "Reddit": "https://www.reddit.com/user/{}",
     # E-commerce & Marketplace
     "eBay": "https://www.ebay.com/usr/{}",
-    "Etsy": "https://www.etsy.com/shop/{}",
     "Fiverr": "https://www.fiverr.com/{}",
     "Amazon": "https://www.amazon.com/gp/profile/amzn1.account.{}",
     # Educational Platforms
     "Khan Academy": "https://www.khanacademy.org/profile/{}",
     "Coursera": "https://www.coursera.org/user/{}",
-    "edX": "https://www.edx.org/user/{}",
-    "Udemy": "https://www.udemy.com/user/{}",
     # Professional & Business Networks
     "Crunchbase": "https://www.crunchbase.com/person/{}",
     "GitHub Gist": "https://gist.github.com/{}",
     "GitHub": "https://www.github.com/{}",
+    "Gitstar Ranking": "https://gitstar-ranking.com/{}",
     "GitLab": "https://gitlab.com/{}",
     "Bitbucket": "https://bitbucket.org/{}",
     "Behance": "https://www.behance.net/{}",
@@ -34,19 +30,21 @@ sites = {
     "Flickr": "https://www.flickr.com/people/{}",
     "Vimeo": "https://vimeo.com/{}",
     "SoundCloud": "https://soundcloud.com/{}",
-    "500px": "https://500px.com/{}",
+    "500px": "https://500px.com/p/{}?view=photos",
     # Blogging & Writing Platforms
-    "Blogger": "https://www.blogger.com/profile/{}",
+    "Blogger": "https://{}.blogspot.com",
     "Tumblr": "https://{}.tumblr.com/",
     "Medium": "https://medium.com/@{}",
-    "Ghost": "https://{}.ghost.io",
     "Wix": "https://{}.wixsite.com/website",
     "Weebly": "https://{}.weebly.com",
     "WordPress": "https://{}.wordpress.com",
+    "WordPressOrg": "https://profiles.wordpress.org/{}",
+    "Weblate": "https://hosted.weblate.org/user/{}",
     "LiveJournal": "https://{}.livejournal.com",
     "BuzzFeed": "https://buzzfeed.com/{}",
     # Other Platforms
-    "Gravatar": "https://en.gravatar.com/{}",
+    "Houzz": "https://houzz.com/user/{}",
+    "Gravatar": "https://gravatar.com/{}",
     "SlideShare": "https://www.slideshare.net/{}",
     "HubPages": "https://hubpages.com/@{}",
     "Wikipedia": "https://en.wikipedia.org/wiki/User:{}",
@@ -57,32 +55,27 @@ sites = {
     "Quora": "https://www.quora.com/profile/{}",
     "Wattpad": "https://www.wattpad.com/user/{}",
     # Programming & Coding Platforms
-    "CodePen": "https://codepen.io/{}",
     "LeetCode": "https://leetcode.com/{}",
-    "Exercism": "https://exercism.io/profiles/{}",
     "Coderbyte": "https://www.coderbyte.com/profile/{}",
     "Codecademy": "https://www.codecademy.com/profiles/{}",
     # Gaming & Streaming Platforms
     "Steam": "https://steamcommunity.com/id/{}",
+    "SteamGroup": "https://steamcommunity.com/groups/{}",
+    "SourceForge": "https://sourceforge.net/u/{}",
     "Twitch": "https://www.twitch.tv/{}",
-    "PlayStation": "https://my.playstation.com/profile/{}",
-    "Nintendo": "https://en-americas-support.nintendo.com/app/social-profile",
-    "Origin": "https://www.origin.com/usa/en-us/profile/{}/",
-    "Epic Games": "https://www.epicgames.com/account/personal?productName=&lang=en_US",
+    "Nintendo Life": "https://www.nintendolife.com/users/{}",
     "GOG": "https://www.gog.com/u/{}",
-    "Ubisoft": "https://club.ubisoft.com/en-US/profile/{}",
     "Spotify": "https://open.spotify.com/user/{}",
     # Additional Platforms
-    "Wellfound": "https://wellfound.com/u/{}",
+    "PyPi": "https://pypi.org/user/{}",
     "SlideShare": "https://www.slideshare.net/{}",
+    "Slides": "https://slides.com/{}",
     "F6S": "https://www.f6s.com/{}",
     "Dribbble": "https://dribbble.com/{}",
-    "Angel.co": "https://angel.co/{}",
     "Gravatar": "https://en.gravatar.com/{}",
     "SpeakerDeck": "https://speakerdeck.com/{}",
     "Behance": "https://www.behance.net/{}",
     "Academia.edu": "https://{}.academia.edu",
-    "ResearchGate": "https://www.researchgate.net/profile/{}",
     "Vimeo": "https://vimeo.com/{}",
     "Keybase": "https://keybase.io/{}",
     "Bitbucket": "https://bitbucket.org/{}",
@@ -105,27 +98,123 @@ sites = {
     "MySpace": "https://www.myspace.com/{}",
     "MyAnimeList": "https://myanimelist.net/profile/{}",
     "BuyMeACoffee": "https://www.buymeacoffee.com/{}",
+    "Freelancer": "https://www.freelancer.com/u/{}",
+    "Mapify": "https://mapify.travel/{}",
+    "Pastebin": "https://pastebin.com/u/{}",
+    "Mydramalist": "https://www.mydramalist.com/profile/{}",
+    "Scribd": "https://www.scribd.com/{}",
+    "Lichess": "https://lichess.org/@/{}",
+    "Chess": "https://www.chess.com/member/{}",
+    "Osu!": "https://osu.ppy.sh/users/{}",
+    "Fixya": "https://www.fixya.com/users/{}",
+    "Issuu": "https://issuu.com/{}",
+    "Venmo": "https://account.venmo.com/u/{}",
+    "ThemeForest": "https://themeforest.net/user/{}",
+    "TradingView": "https://www.tradingview.com/u/{}",
+    "TETR.IO": "https://ch.tetr.io/u/{}",
+    "Scratch": "https://scratch.mit.edu/users/{}",
+    "RuneScape": "https://apps.runescape.com/runemetrics/app/overview/player/{}",
+    "Rumble": "https://rumble.com/user/{}",
+    "Roblox": "https://www.roblox.com/user.aspx?username={}",
+    "Replit.com": "https://replit.com/@{}",
+    "Pokemon Showdown": "https://pokemonshowdown.com/users/{}",
+    "OpenStreetMap": "https://www.openstreetmap.org/user/{}",
+    "Monkeytype": "https://monkeytype.com/profile/{}",
+    "Linktree": "https://linktr.ee/{}",
+    "Kongregate": "https://www.kongregate.com/accounts/{}",
+    "Instructables": "https://www.instructables.com/member/{}",
+    "Imgur": "https://imgur.com/user/{}",
+    "HackerRank": "https://hackerrank.com/{}",
+    "GaiaOnline": "https://www.gaiaonline.com/profiles/{}",
+    "Flipboard": "https://flipboard.com/{}",
+    "Disqus": "https://disqus.com/by/{}",
+    "Audiojungle": "https://audiojungle.net/user/{}",
+    "Apple Discussions": "https://discussions.apple.com/profile/{}",
+    "Anilist": "https://anilist.co/user/{}",
+    "note": "https://note.com/{}",
+    "Minds": "https://www.minds.com/{}",
+    "uID": "http://uid.me/{}",
+    "Interpals": "https://www.interpals.net/{}",
+    "Geocaching": "https://www.geocaching.com/p/default.aspx?u={}",
+    "Fur Affinity": "https://www.furaffinity.net/user/{}",
+    "devRant": "https://devrant.com/users/{}",
+    "freeCodeCamp": "https://www.freecodecamp.org/{}",
+    "Couchsurfing!": "https://www.couchsurfing.com/people/{}",
+    "YouPic": "https://youpic.com/photographer/{}",
+    "Unsplash": "https://unsplash.com/@{}",
+    "Ultimate-Guitar": "https://ultimate-guitar.com/u/{}",
+    "TryHackMe": "https://tryhackme.com/p/{}",
+    "Trello": "https://trello.com/{}",
+    "Trakt": "https://www.trakt.tv/users/{}",
+    "Periscope": "https://www.periscope.tv/{}",
+    "Polarsteps": "https://polarsteps.com/{}",
+    "Naver": "https://blog.naver.com/{}",
+    "Hackaday": "https://hackaday.io/{}",
+    "Choice Community": "https://choice.community/u/{}/summary",
+    "Clapper": "https://clapperapp.com/{}",
+    "Bikemap": "https://www.bikemap.net/en/u/{}/routes/created/",
+    "BioHacking": "https://forum.dangerousthings.com/u/{}",
 }
 
 # indicators for false positive 200 responses
 soft404_indicators = [
     "This profile could not be found",
-    "Sorry, this user was not found.",
-    "Page not available",
+    "Sorry, this user was not found",
     "Page Not Found",
     "the profile was either removed ",
     "The specified profile could not be found",
-    # for wordpress
     "doesn&apos;t&nbsp;exist",
     "This page doesn't exist",
     "404 Not Found",
+    "Couldn't find this account",
+    "Sorry, nobody on Reddit goes by that name",
+    "The person may have been banned or the username is incorrect.",
+    "Some error occured while loading page for you. Please try again",
+    "No longer a registered user",
+    "No user ID specified or user does not exist!",
+    "user does not exist",
+    "An error was encountered while processing your request",
+    "Sorry, the page you were looking for doesn’t exist",
+    "Wikipedia does not have a",
 ]
 
+user_agents = [
+    # Chrome 115.0 on macOS
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    # Chrome 114.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    # Chrome 118.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+    # Chrome 119.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    # Firefox 116.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+    # Firefox 115.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    # Firefox 119.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
+    # Chrome 114.0 on macOS
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    #  Chrome 115.0 on Linux
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    # Chrome 116.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    # Firefox 115.0 on Linux
+    "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    # Firefox 116.0 on Linux
+    "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
+    # Edge 115.0 on Windows
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
+]
 
 if __name__ == "__main__":
-    from rich import print as rprint
+    # from rich import print as rprint
 
-    for site, url in sites.items():
-        rprint(f"[bright_green]{site}: [blue]{url}")
+    # for site, url in sites.items():
+    #     rprint(f"[bright_green]{site}: [blue]{url}")
 
-    rprint(f"\n[magenta]{len(sites)} Sites")
+    # rprint(f"\n[magenta]{len(sites)} Sites")
+    from sagemode import Sagemode
+
+    sage = Sagemode("ranniel")
+    sage.check_site("Site", sites['HackerRank'])
